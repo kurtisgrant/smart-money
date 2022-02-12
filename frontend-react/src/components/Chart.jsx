20f1b1cbc7e3108645851788f77ccbb8819da1c5
Robert <robgladue@hotmail.com>
2022-02-12 13:05:16 -0600

    add Chart component and corresponding css file

---
+++ frontend-react/src/components/Chart.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Chart.css';
+
+
+const Chart = () => {
+  return(
+    <div>
+      <div className="data-chart">
+        
+      </div>
+      <div className="chart-btns">
+        <button className="btn-play">Play</button>
+        <button className="btn-pause">Pause</button>
+      </div>
+    </div>
+  )
+}
+
+export default Chart;
