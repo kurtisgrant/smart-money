b990082b59ce7a142c358ae5beb26750f63f3844
Kurtis Grant <kurtis.b.grant@gmail.com>
2022-02-13 17:45:45 -0500

    Update "crunch.js" to generate cleaned S&P500 historical data

--- market-data/crunch.js
+++ market-data/crunch.js
@@ -3,22 +3,50 @@ const fs = require('fs');
 
 const results = [];
 const csvFileName = process.argv[2];
-const jsonFileName = process.argv[2].split('.')[0] + '.json';
+const jsonFileName = Math.random().toString().slice(2, 6) + process.argv[2].split('.')[0] + '.json';
 
 fs.createReadStream(csvFileName)
   .pipe(csv({}))
   .on('data', data => results.push(data))
   .on('end', () => {
+
+    const newData = results.map((row) => {
+      return {
+        date: row.Date,
+        price: row.SP500,
+        yr2000Price: row['Consumer Price Index']
+      };
+    });
+
+
+
     console.log("Data Preview: \n\n");
-    console.log(results[0]);
-    console.log(results[1]);
+    console.log(newData[0]);
+    console.log(newData[1]);
     console.log("    ...");
-    console.log(results[results.length - 2]);
-    console.log(results[results.length - 1]);
+    console.log(newData[newData.length - 2]);
+    console.log(newData[newData.length - 1]);
     console.log("Done. Find your file named: ", jsonFileName);
+
+
+
+
     fs.writeFileSync(
       jsonFileName,
-      JSON.stringify(results)
+      JSON.stringify(newData)
     );
   });
 
+
+// {
+//   Date: '1879-04-01',
+//   SP500: '3.77',
+//   Dividend: '0.19',
+//   Earnings: '0.33',
+//   'Consumer Price Index': '8.18',
+//   'Long Interest Rate': '4.17',
+//   'Real Price': '115.11',
+//   'Real Dividend': '5.7',
+//   'Real Earnings': '10.18',
+//   PE10: ''
+// },
