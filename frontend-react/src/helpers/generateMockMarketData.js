9bb7674283c01899e8052e9ddc6018a55e51fef7
Kurtis Grant <kurtis.b.grant@gmail.com>
2022-02-13 18:06:38 -0500

    Setup for helper function to generate mock market data

---
+++ frontend-react/src/helpers/generateMockMarketData.js
@@ -0,0 +1,12 @@
+import { deviation, mean } from 'd3';
+import sp500 from '../data/parsed_SP500';
+
+const generateMarketData = (firstYear, lastYear) => {
+
+};
+
+const getDataGeneratorInputVariables = (firstYear, lastYear) => {
+
+};
+
+export default generateMarketData;
