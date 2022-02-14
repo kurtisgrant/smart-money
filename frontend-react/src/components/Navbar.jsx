c5f322120c2efef1cc6ae67b1893971ff7d69730
Kurtis Grant <kurtis.b.grant@gmail.com>
2022-02-14 09:17:45 -0500

    Update Navbar.js to render user name and logout button conditionally if user is logged in

--- frontend-react/src/components/Navbar.jsx
+++ frontend-react/src/components/Navbar.jsx
@@ -3,17 +3,20 @@ import './Navbar.scss';
 import Button from './Button';
 
 function Navbar({ appState, logout }) {
+
+	const authUserDiv = <div className="auth-user">
+		<div className="user-name">
+			<div>{appState.user?.name}</div>
+		</div>
+		<Button white onClick={() => logout()}>Logout</Button>
+	</div>;
+
 	return (
 		<div className="navbar">
 			<div className="logo">
 				<h1>Logo</h1>
 			</div>
-			<div className="auth-user">
-				<div className="user-name">
-					<div>{appState.user ? appState.user.name : null}</div>
-				</div>
-				<Button white onClick={() => logout()}>Logout</Button>
-			</div>
+			{appState.user && authUserDiv}
 		</div>
 	);
 }
