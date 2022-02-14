98dbe32190ea5bb3c41bdd8f935536c7e86235c7
Kurtis Grant <kurtis.b.grant@gmail.com>
2022-02-14 09:06:19 -0500

    Persist user to session storage in App.js with useEffect

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import './App.scss';
 import Navbar from './components/Navbar';
@@ -19,12 +19,23 @@ generateMarketData({
 function App() {
   const [appState, setAppState] = useState({ user: null });
   const setUser = (user) => {
+    sessionStorage.setItem('user', JSON.stringify(user));
     setAppState({ ...appState, user });
   };
+  const logout = () => {
+    sessionStorage.clearItem('user')
+    setAppState({ ...appState, user: null})
+  }
+
+  useEffect(() => {
+    const sessionUser = JSON.parse(sessionStorage.getItem('user'));
+    console.log('Setting user from session storage: ', sessionUser);
+    sessionUser && setUser(sessionUser);
+  }, []);
 
   return (
     <div className="App">
-      <Navbar appState={appState} />
+      <Navbar appState={appState} logout={logout} />
       <Routes>
         <Route path="/" element={<IndexContainer user={appState.user} setUser={setUser} />} />
         <Route path="/sim/:simulation_key" element={<SimulationContainer user={appState.user} setUser={setUser} />} />
