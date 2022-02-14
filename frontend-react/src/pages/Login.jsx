f0ff35c20dc730d3f836962379b21b5bfde9326b
Jeffery Park <jp@Jefferys-MBP.cogeco.local>
2022-02-13 20:48:23 -0500

    Add login form and style

--- frontend-react/src/pages/Login.jsx
+++ frontend-react/src/pages/Login.jsx
@@ -1,17 +1,28 @@
 import React from 'react';
+import Button from '../components/Button';
+import './Login.scss'
 
 function Login({ setUser }) {
-  return (
-    <div>
-      <h2>Login Page</h2>
-      <form action="">
-        <label htmlFor="email">Email</label>
-        <input id="email" type="text" />
-        <button onClick={() => setUser({id: 3, name: 'Mrs. Krabappel', type: 'teacher'})}>Login</button>
-        <p>(Just click login to login as Mrs. K)</p>
-      </form>
-    </div>
-  );
+	return (
+		<div className="container">
+			<header>
+				<h2>Login</h2>
+			</header>
+			<form action="">
+				<label htmlFor="email">Email</label>
+				<input id="email" type="text" placeholder="Enter Email" />
+				<Button
+					green
+					onClick={() =>
+						setUser({ id: 3, name: 'Mrs. Krabappel', type: 'teacher' })
+					}
+				>
+					Login
+				</Button>
+				<p>(Just click login to login as Mrs. K)</p>
+			</form>
+		</div>
+	);
 }
 
 export default Login;
