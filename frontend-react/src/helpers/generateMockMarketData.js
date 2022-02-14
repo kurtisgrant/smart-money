55e518a1689219d701945b920e45d8cd0592b67e
Kurtis Grant <kurtis.b.grant@gmail.com>
2022-02-13 20:52:32 -0500

    Implement function for calculating mean & standard deviation for market data generator

--- frontend-react/src/helpers/generateMockMarketData.js
+++ frontend-react/src/helpers/generateMockMarketData.js
@@ -1,12 +1,37 @@
 import { deviation, mean } from 'd3';
 import sp500 from '../data/parsed_SP500';
 
-const generateMarketData = (firstYear, lastYear) => {
+const generateMarketDataFromYrRange = (firstYear, lastYear) => {
+  const data = sp500.filter(row => {
+    const year = Number(row.date.slice(0, 4));
+    return (year >= firstYear && year <= lastYear);
+  });
+
+  const [
+    monthlyMeanChange, 
+    monthlyStdDevChange
+  ] = getMonthlyMeanAndStdDevChanges(data);
+
+  return 'todo'
 
 };
 
-const getDataGeneratorInputVariables = (firstYear, lastYear) => {
+const getMonthlyMeanAndStdDevChanges = (data) => {
+
+  let marketMonthPcntChanges = [];
+
+  // Start at second month
+  for (let i = 1; i < data.length; i++) {
+    const thisMonthPrice = data[i].price;
+    const prevMonthPrice = data[i - 1].price;
+    marketMonthPcntChanges.push((thisMonthPrice - prevMonthPrice) / prevMonthPrice);
+  }
+
+  const meanMonthlyChange = mean(marketMonthPcntChanges)
+  const stdDevMonthlyChange = deviation(marketMonthPcntChanges)
+
+  return [meanMonthlyChange, stdDevMonthlyChange]
 
 };
 
-export default generateMarketData;
+export default generateMarketDataFromYrRange;
