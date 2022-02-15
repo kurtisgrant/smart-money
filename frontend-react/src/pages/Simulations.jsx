2ddbfcc7c069e487188f7f0f533488bfa362c4d0
Kurtis Grant <kurtis.b.grant@gmail.com>
2022-02-14 23:13:22 -0500

    Add button to simulations index page linking to new simulation page with react router

--- frontend-react/src/pages/Simulations.jsx
+++ frontend-react/src/pages/Simulations.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import Button from '../components/Button';
+import { Link } from 'react-router-dom';
 
 function Simulations({ user }) {
   return (
     <div>
       <p>Welcome back {user.name}!</p>
       <h1>My Simulations</h1>
+      <Link to='/new'><Button green>New Simulation</Button></Link>
       <p>Simulations list will go here</p>
     </div>
   );
