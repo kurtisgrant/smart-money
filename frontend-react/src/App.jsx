59966b31fb3d30894cbdc20c815f191c7c96f6b1
Kurtis Grant <kurtis.b.grant@gmail.com>
2022-02-14 23:15:10 -0500

    Remove test of mock market data function call from App.jsx

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -4,17 +4,7 @@ import './styles/App.scss';
 import Navbar from './components/Navbar';
 import IndexContainer from './containers/IndexContainer';
 import SimulationContainer from './containers/SimulationContainer';
-
-// Temp for testing
-// (See console in browser to view data)
-import generateMarketData from './helpers/generateMockMarketData';
-generateMarketData({
-  firstYearSeed: 1915, // Min: 1871
-  lastYearSeed: 2015, // Max: 2018
-  adjustForInflation: true, // true: Adjust to Today's dollars, false: Allow inflation
-  startPrice: 10, // First price in fake market data
-  months: 720, // How many months of data do you want?
-});
+import NewSimulation from './pages/NewSimulation';
 
 function App() {
   const [appState, setAppState] = useState({ user: null });
@@ -40,6 +30,7 @@ function App() {
         <Routes>
           <Route path="/" element={<IndexContainer user={appState.user} setUser={setUser} />} />
           <Route path="/sim/:simulation_key" element={<SimulationContainer user={appState.user} setUser={setUser} />} />
+          <Route path="/new" element={<NewSimulation />} />
         </Routes>
       </div>
     </div>
