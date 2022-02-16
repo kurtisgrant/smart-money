315018cf1b89600a49e4d50a66d42db2c3b8df0c
Jeffery Park <jp@Jefferys-MBP.cogeco.local>
2022-02-15 22:42:07 -0500

    Update teacher simulation control panel page

--- frontend-react/src/pages/SimulationControlPanel.jsx
+++ frontend-react/src/pages/SimulationControlPanel.jsx
@@ -1,19 +1,32 @@
 import React, { useContext } from 'react';
-import { useParams } from 'react-router-dom'
+import { useParams } from 'react-router-dom';
 import { UserContext } from '../UserContext';
 import LineChart from '../components/LineChart';
-import Studentlist from '../components/StudentList';
+import StudentListBalance from '../components/StudentListBalance';
+import Button from '../components/Button';
+import './SimulationControlPanel.scss';
 
 function SimulationControlPanel() {
-  const { simulationKey } = useParams()
-  const { user } = useContext(UserContext);
+	const { simulationKey } = useParams();
+	const { user } = useContext(UserContext);
 
-  return (
-    <div>SimulationControlPanel
-      {/* <LineChart /> */}
-      {/* <Studentlist /> */}
-    </div>
-  );
+	return (
+		<div className="simulation-control-panel-container">
+			<div className="simulation-control-panel">
+				<LineChart />
+				<div className="simulation-run-buttons">
+					<Button green>Play</Button>
+					<Button white>Pause</Button>
+				</div>
+			</div>
+			<div className="simulation-control-panel-student-list">
+				<div className="list-heading">
+					<h2>Students</h2>
+				</div>
+				<StudentListBalance />
+			</div>
+		</div>
+	);
 }
 
 export default SimulationControlPanel;
