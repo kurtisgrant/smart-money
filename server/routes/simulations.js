3db6ba39a2ce70b8fd64973cd4d77cc101aab7ac
Jeffery Park <jp@Jefferys-MBP.cogeco.local>
2022-02-16 20:27:28 -0500

    Add feature to delete simulation list in db

--- server/routes/simulations.js
+++ server/routes/simulations.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 
 module.exports = (db) => {
+  // show list of all simulations
   router.get('/', (req, res) => {
     const query = "SELECT * FROM simulations";
     
@@ -9,6 +10,7 @@ module.exports = (db) => {
     .catch(e => console.log(e.message))
   });
 
+  // create new simulation
   router.post('/', (req, res) => {
     const { inputOne, inputTwo, id } = req.body;
     const query = `
@@ -20,9 +22,20 @@ module.exports = (db) => {
     db.query(query, [inputOne, inputTwo, id])
   });
 
+  // delete simulation
+  router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+
+    const query = `
+    DELETE FROM simulations
+    WHERE id = $1`
+
+    db.query(query, [id])
+  });
+
+  // show list of simulations for specific teacher
   router.get('/list/:teacherId', (req, res) => {
     const { teacherId } = req.params;
-    console.log('list:', req.params);
 
     const query = `
     SELECT id, name, created_date AS date FROM simulations
