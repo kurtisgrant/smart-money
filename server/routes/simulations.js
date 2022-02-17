8187ce6068c221bf3cf9b23e3da6233dc0b1e978
Jeffery Park <jp@Jefferys-MBP.cogeco.local>
2022-02-17 14:53:36 -0500

    Add api route to receive and create simulation and students data

--- server/routes/simulations.js
+++ server/routes/simulations.js
@@ -1,57 +1,90 @@
 const router = require('express').Router();
 
 module.exports = (db) => {
-  // show list of all simulations
-  router.get('/', (req, res) => {
-    const query = "SELECT * FROM simulations";
+	// show list of all simulations
+	router.get('/', (req, res) => {
+		const query = 'SELECT * FROM simulations';
 
-    db.query(query)
-      .then(data => res.json(data.rows))
-      .catch(e => console.log(e.message));
-  });
+		db.query(query)
+			.then((data) => res.json(data.rows))
+			.catch((e) => console.log(e.message));
+	});
 
-  // create new simulation
-  router.post('/', (req, res) => {
-    console.log(req.body)
-    let { className, studentIncome, studentExpense, randomMarketData } = req.body;
-    
-    // convert dollars to cents
-    studentIncome *= 100;
-    studentExpense *= 100;
+	// create new simulation
+	router.post('/', (req, res) => {
+		console.log(req.body);
+		let {
+			simulationName,
+			simulationKey,
+			studentIncome,
+			studentExpense,
+			randomMarketData,
+			teacherId,
+			students,
+		} = req.body;
 
-    const query = `
-    INSERT INTO simulations(name, simulation_key, , mock_market_data, income, expense, teacher_id)
+		// convert dollars to cents
+		studentIncome *= 100;
+		studentExpense *= 100;
+
+		// insert simulation info to simulations table
+		const querySimulations = `
+    INSERT INTO simulations(name, simulation_key, mock_market_data, income, expense, teacher_id)
     VALUES ($1, $2, $3, $4, $5, $6)
     RETURNING *
     `;
 
-    db.query(query, [inputOne, inputTwo, id]);
-  });
+		db.query(querySimulations, [
+			simulationName,
+			simulationKey,
+			randomMarketData,
+			studentIncome,
+			studentExpense,
+			teacherId,
+		])
+			.then((res) => {
+				const simulationId = res.rows[0].id;
 
-  // delete simulation
-  router.delete('/:id', (req, res) => {
-    const { id } = req.params;
+				for (const student of students) {
+					const queryStudents = `
+            INSERT INTO students(name, access_code, simulation_id)
+            VALUES ($1, $2, $3)
+          `;
 
-    const query = `
+					db.query(queryStudents, [
+						student.name,
+						student.accessCode,
+						simulationId,
+					]);
+				}
+			})
+			.catch((err) => console.log(err.message));
+	});
+
+	// delete simulation
+	router.delete('/:id', (req, res) => {
+		const { id } = req.params;
+
+		const query = `
     DELETE FROM simulations
     WHERE id = $1`;
 
-    db.query(query, [id]);
-  });
+		db.query(query, [id]);
+	});
 
-  // show list of simulations for specific teacher
-  router.get('/list/:teacherId', (req, res) => {
-    const { teacherId } = req.params;
+	// show list of simulations for specific teacher
+	router.get('/list/:teacherId', (req, res) => {
+		const { teacherId } = req.params;
 
-    const query = `
+		const query = `
     SELECT id, name, created_date AS date, simulation_key FROM simulations
     WHERE teacher_id = $1
     `;
 
-    db.query(query, [teacherId])
-      .then(data => res.json(data.rows))
-      .catch(e => console.log(e.message));
-  });
+		db.query(query, [teacherId])
+			.then((data) => res.json(data.rows))
+			.catch((e) => console.log(e.message));
+	});
 
-  return router;
+	return router;
 };
