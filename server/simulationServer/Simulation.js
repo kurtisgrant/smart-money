49b008178c41f7736e039a591c443b7f8fc9142c
Kurtis Grant <kurtis.b.grant@gmail.com>
2022-02-20 23:58:19 -0500

    Add mountStudentSockets method to Simulation model

--- server/simulationServer/Simulation.js
+++ server/simulationServer/Simulation.js
@@ -20,15 +20,30 @@ class Simulation {
     const dbH = this.dbHelpers;
     const dbStudentsAndAccounts = await dbH.getStudentsAndAccounts(this.simId);
     const newStudentsObj = {};
-    dbStudentsAndAccounts.forEach(async student => {
+
+    // Loop through students, adding their individual market transactions to each
+    for (let i = 0; i < dbStudentsAndAccounts.length; i++) {
+      const student = dbStudentsAndAccounts[i];
       const studentMarketTransactions = await dbH.getMarketTransactionsForStudent(student.stuId);
       student.marketTransactions = studentMarketTransactions;
       newStudentsObj[student.stuId] = student;
-    });
+    }
     this.students = newStudentsObj;
     return;
   }
 
+  async mountStudentSockets() {
+    const io = this.io;
+    const dbH = this.dbHelpers;
+    const studentSimRoom = 's-' + this.simKey;
+    const ioSocketsForSim = await io.in(studentSimRoom).fetchSockets();
+    if (!ioSocketsForSim.length) return;
+    for (const socket of ioSocketsForSim) {
+      const stuId = socket.user.id;
+      this.students[stuId].socket = socket;
+    }
+    return;
+  }
 }
 
 module.exports = Simulation;
--- server/simulationServer/index.js
+++ server/simulationServer/index.js
@@ -4,12 +4,23 @@ const Simulation = require('./Simulation');
 
 
 module.exports = (io, db) => {
-  const { getSimulationId, getSimulationById, getTeacherForSimulation, toggleIsPlaying, getRunningSimulations } = dbHelpers(db);
+  const {
+    getSimulationId,
+    getSimulationById,
+    getTeacherForSimulation,
+    toggleIsPlaying,
+    getRunningSimulations,
+    getSimulationKey,
+    getMarketTransactionsForStudent
+  } = dbHelpers(db);
+
+  // This will hold the interval allowing us to clear(stop) it later
   let serveClientsInterval;
+  // Object of simulation models currently loaded into memory
   const loadedSimulations = {};
 
   io.on('connection', (socket) => {
-    console.log('\n🟢 client connected. socket id: ', socket.id);
+    console.log('\n🟢  Client connected. socket id: ', socket.id);
     if (!serveClientsInterval) {
       startServingClients();
     }
@@ -17,10 +28,29 @@ module.exports = (io, db) => {
       socket.user = user;
       socket.isTeacher = user.type === 'teacher';
       socket.isStudent = user.type === 'student';
-      // user.type === 'student' && socket.simulatio
       console.log(
-        `👤   ${user.name} has been set as the socket user.
+        `👤  ${user.name} has been set as the socket user.
         ↳ user obj: `, user);
+
+      if (socket.isStudent) {
+        // Students join room for their simulation immediately
+        // since they can never join other simulaitons
+        const simKey = await getSimulationKey(socket.user.simulation_id);
+        const studentSimRoom = 's-' + simKey;
+        socket.join(studentSimRoom);
+      }
+    });
+
+    socket.on('JOIN_SIMULATION', simKey => {
+      const teacherSimRoom = 't-' + simKey;
+      socket.join(teacherSimRoom);
+      console.log(`\n🚪=>  ${socket.user?.name} joined simulation with key: ${simKey}`);
+    });
+
+    socket.on('LEAVE_SIMULATION', simKey => {
+      const teacherSimRoom = 't-' + simKey;
+      socket.leave(teacherSimRoom);
+      console.log(`\n🚪<=  ${socket.user?.name} left simulation with key: ${simKey}`);
     });
 
     socket.on('PLAY_PAUSE_SIMULATION', async (simulationKey) => {
@@ -31,23 +61,20 @@ module.exports = (io, db) => {
 
       // Toggle isPlaying & send new isPlaying state
       const dbUpdatedRow = await toggleIsPlaying(simId);
-      const newIsPlaying = dbUpdatedRow.is_playing;
-      console.log(`\n⏯  ${socket.user.name} ${newIsPlaying ? 'started' : 'paused'} simulation ${simId} (${simulationKey})`);
-      socket.emit('PLAY_PAUSE_UPDATE', newIsPlaying);
+      const simWasStarted = dbUpdatedRow.is_playing;
+      console.log(`\n⏯  ${socket.user.name} ${simWasStarted ? 'started' : 'paused'} simulation ${simId} (${simulationKey})`);
+      socket.emit('PLAY_PAUSE_UPDATE', simWasStarted);
 
       // If simulation has been started, ensure it is in loadedSimulations
-      if (newIsPlaying && !Object.keys(loadedSimulations).includes(simId)) {
-        const dbSimulation = await getSimulationById(simId);
-        loadedSimulations[simId] = new Simulation(db, io, dbSimulation);
+      if (simWasStarted) {
+        simulationStartHandler(simId);
       }
-
     });
 
     socket.on('disconnect', (reason) => {
       console.log(
-        `\n❌ client disconnected. socket id: ${socket.id}
-        ↳ reason: ${reason}
-        ↳ user was: ${socket.data.user?.name || 'not logged in'}`);
+        `\n❌  ${socket.user?.name} was disconnected. socket id: ${socket.id}
+        ↳ reason: ${reason}`);
       updateSocketsCount();
     });
 
@@ -81,9 +108,24 @@ module.exports = (io, db) => {
     }
   }
 
+  async function simulationStartHandler(simId) {
+
+    // If simulation wasn't in loadedSimulations, add it
+    if (!Object.keys(loadedSimulations).includes(String(simId))) {
+      console.log(`💾  Adding simulation  ${simId}  to loadedSimulations object.`);
+      const dbSimulation = await getSimulationById(simId);
+      const newSimModel = new Simulation(db, io, dbSimulation);
+      loadedSimulations[simId] = newSimModel;
+      await newSimModel.setAllStudentData();
+      await newSimModel.mountStudentSockets();
+    }
+    return;
+  }
+
   async function updateLoadedSimulations() {
     const runningSims = await getRunningSimulations();
     const loadedSims = Object.keys(loadedSimulations);
+    return;
   }
 
   async function updateSocketsCount() {
