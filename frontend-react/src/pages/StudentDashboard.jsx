7a287606a7e932d8d0afc87dd4597e161f046212
Jeffery Park <jp@Jefferys-MBP.cogeco.local>
2022-02-20 23:08:42 -0500

    Add simulation graph

--- frontend-react/src/pages/StudentDashboard.jsx
+++ frontend-react/src/pages/StudentDashboard.jsx
@@ -2,6 +2,7 @@ import React, { useState, useContext, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { UserContext } from '../UserContext';
 import { SocketContext } from '../SocketContext';
+import LineChartStudent from '../components/LineChartStudent';
 import './StudentDashboard.scss';
 import axios from 'axios';
 
@@ -9,6 +10,8 @@ function StudentDashboard() {
 	const { simulationKey } = useParams();
 	const { user } = useContext(UserContext);
 	const { socket } = useContext(SocketContext);
+	const [marketData, setMarketData] = useState([]);
+	const [currentMonth, setCurrentMonth] = useState(0);
 	const [income, setIncome] = useState(0);
 	const [expense, setExpense] = useState(0);
 	const [surplus, setSurplus] = useState(0);
@@ -16,34 +19,39 @@ function StudentDashboard() {
 	const [investments, setInvestments] = useState(0);
 	const [chequings, setChequings] = useState(0);
 
-  useEffect(() => {
-    axios
-    .get(`/api/simulations/cashflow/${simulationKey}`)
-    .then((res) => {
-      const incomeData = Number(res.data[0].income) / 100;
-      const expenseData = Number(res.data[0].expense) / 100;
-      const surplusData = incomeData - expenseData
+	useEffect(() => {
+		axios
+			.get(`/api/simulations/cashflow/${simulationKey}`)
+			.then((res) => {
+				const incomeData = Number(res.data[0].income) / 100;
+				const expenseData = Number(res.data[0].expense) / 100;
+				const surplusData = incomeData - expenseData;
 
-      setIncome(incomeData);
-      setExpense(expenseData);
-      setSurplus(surplusData);
-      setChequings(surplusData);
-    })
-    .catch((err) => console.log(err.message));
-    
-  }, [])
+				setIncome(incomeData);
+				setExpense(expenseData);
+				setSurplus(surplusData);
+				setChequings(surplusData);
+			})
+			.catch((err) => console.log(err.message));
 
-  useEffect(() => {
-    setChequings(surplus - savings - investments);
+		axios
+			.get(`/api/simulations/marketdata/${simulationKey}`)
+			.then((res) => setMarketData(JSON.parse(res.data[0].mock_market_data)))
+			.catch((err) => console.log(err.message));
+	}, []);
+
+	useEffect(() => {
+		setChequings(surplus - savings - investments);
 
 		const studentId = user.id;
 
-		const monthlyAllocations = { savings, investments }
-		
-		axios.put(`/api/students/allocations/${studentId}`, monthlyAllocations)
+		const monthlyAllocations = { savings, investments };
+
+		axios
+			.put(`/api/students/allocations/${studentId}`, monthlyAllocations)
 			.then()
-			.catch()
-  }, [savings, investments, surplus])
+			.catch();
+	}, [savings, investments, surplus]);
 
 	const decreaseSavingsAmount = () => {
 		if (savings > 0) {
@@ -52,26 +60,26 @@ function StudentDashboard() {
 	};
 
 	const increaseSavingsAmount = () => {
-    if (savings + 50 + investments > surplus) {
-      return
-    }
+		if (savings + 50 + investments > surplus) {
+			return;
+		}
 
-    setSavings(savings + 50);
+		setSavings(savings + 50);
 	};
 
-  const decreaseInvestmentsAmount = () => {
-    if (investments > 0) {
+	const decreaseInvestmentsAmount = () => {
+		if (investments > 0) {
 			setInvestments(investments - 50);
 		}
-  }
+	};
 
-  const increaseInvestmentsAmount = () => {
-    if (investments + 50 + savings > surplus) {
-      return
-    }
-    
-    setInvestments(investments + 50);
-  }
+	const increaseInvestmentsAmount = () => {
+		if (investments + 50 + savings > surplus) {
+			return;
+		}
+
+		setInvestments(investments + 50);
+	};
 
 	useEffect(() => {
 		if (!socket) return;
@@ -104,7 +112,9 @@ function StudentDashboard() {
 						<span className="col-2">${surplus.toLocaleString()} / m</span>
 					</div>
 				</div>
-				<div className="dashboard-top-right"></div>
+				<div className="dashboard-top-right">
+					<LineChartStudent marketData={marketData} />
+				</div>
 			</div>
 			<div className="dashboard-bottom">
 				<div className="dashboard-bottom-left">
@@ -119,7 +129,7 @@ function StudentDashboard() {
 							+
 						</span>
 					</div>
-          <div className="allocation">
+					<div className="allocation">
 						<span className="col-1">Investments</span>
 						<span className="col-2" onClick={decreaseInvestmentsAmount}>
 							-
@@ -129,7 +139,7 @@ function StudentDashboard() {
 							+
 						</span>
 					</div>
-          <div className="allocation chequings">
+					<div className="allocation chequings">
 						<span className="col-1">Chequings</span>
 						<span className="col-2"></span>
 						<span className="col-3">${chequings.toLocaleString()}</span>
